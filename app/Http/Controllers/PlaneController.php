000c02a5726e972dd2e28a5a56c3de165a236fe3
sr-calcetines <pgavilansanc@gmail.com>
2025-01-31 18:52:38 +0100

    feat: model migration and controller structure added for planes, flights and pivot(migration)

---
+++ app/Http/Controllers/PlaneController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Plane;
+use Illuminate\Http\Request;
+
+class PlaneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Plane $plane)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Plane $plane)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Plane $plane)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Plane $plane)
+    {
+        //
+    }
+}
