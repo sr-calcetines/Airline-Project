8ac3d5be63274ab52c45bf4579114c7accc2effc
sr-calcetines <pgavilansanc@gmail.com>
2025-01-31 19:17:48 +0100

    feat: added models

--- app/Models/Flight.php
+++ app/Models/Flight.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use App\Models\Plane;
 
 class Flight extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        "date",
+        "departure",
+        "arrival",
+        "plane_id",
+        "aviable"
+    ];
+
+    public function airplane(): BelongsTo
+    {
+        return $this->belongsTo(Plane::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, "flight_user");
+    }
 }
--- app/Models/Plane.php
+++ app/Models/Plane.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\Flight;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Plane extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "max_capacity",
+        "reserved"
+    ];
+    public function flights(): HasMany
+    {
+        return $this->hasMany(Flight::class);
+    }
 }
