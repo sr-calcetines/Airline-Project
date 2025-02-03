4a1e35a80c272b1a0692e3a17cd34d11e9d7edf2
sr-calcetines <pgavilansanc@gmail.com>
2025-02-03 17:54:53 +0100

    planes and flights factory added

---
+++ database/factories/PlaneFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Plane>
+ */
+class PlaneFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => fake()->word(),
+            "max_capacity" => fake()->numberBetween(100, 200),
+            "reserved" =>fake()->numberBetween(0, 200)
+        ];
+    }
+}
