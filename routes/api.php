7ab4d10a3a36b9b44155e2190dd6558ecd51aaab
sr-calcetines <pgavilansanc@gmail.com>
2025-03-07 13:18:51 +0100

    feat: added api routes (CRUD) for flights and planes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\FlightController;
+use App\Http\Controllers\Api\PlaneController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -18,3 +20,17 @@ Route::group([
     Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
     Route::post('/me', [AuthController::class, 'me'])->middleware('auth:api')->name('me');
 });
+
+//Flights routes
+Route::get('/flights',[FlightController::class,'index'])->name('apiflightall');
+Route::get('/flights/show/{id}',[FlightController::class, 'show'])->name('apiflightshow');
+Route::delete('/flights/destroy/{id}',[FlightController::class, 'destroy'])->name('apiflightdestroy');
+Route::post('/flights/store',[FlightController::class, 'store'])->name('apiflightstore');
+Route::put('/flights/update/{id}',[FlightController::class, 'update'])->name('apiflightupdate');
+
+//Planes routes
+Route::get('/planes',[PlaneController::class,'index'])->name('apiplaneall');
+Route::get('/planes/show/{id}',[PlaneController::class, 'show'])->name('apiplaneshow');
+Route::delete('/planes/destroy/{id}',[PlaneController::class, 'destroy'])->name('apiplanedestroy');
+Route::post('/planes/store',[PlaneController::class, 'store'])->name('apiplanestore');
+Route::put('/planes/update/{id}',[PlaneController::class, 'update'])->name('apiplaneupdate');
